a75b7d8b4d1df3db1ea006e558eb6080d8d14339
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-08-24 19:19:23 -0400

    implemented form validation and alerts

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,25 +1,68 @@
 import './ContactPage.css';
+import { useState } from 'react';
+import { validateEmail } from '../../utils/validation';
 export default function ContactPage(){
+    const [showMessage, setShowMessage] = useState("")
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+       
+    const handleChange = (e)=>{
+       const {target} = e;
+       const inputType = target.name;
+       const inputValue = target.value;
+       if(inputType === "firstName"){
+         setFirstName(inputValue);
+       }
+       if(inputType === "lastName"){
+         setLastName(inputValue);
+       }
+       if(inputType === "email"){
+         setEmail(inputValue);
+       }
+       if(inputType === "message"){
+         setMessage(inputValue);
+       }
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        const validEmail = validateEmail(email);
+        if(!validEmail){
+            setShowMessage( <div className="alert alert-danger" role="alert">Please provide a valid email.</div>)
+        }else if(validEmail && firstName && lastName && message){
+            setShowMessage(<div className="alert alert-success" role="alert">Thank You!!!</div>)
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setMessage("");
+        }
+    
+       
+        
+    }
     return (
         <div className="mx-auto d-flex flex-column justify-content-center align-items-center h-100 form-container">
         <h2>Contact </h2>
+         {showMessage} 
         <div className="card m-5 col-lg-5">
             <h4>Get In Touch</h4>
             <form
                 // action="https://formsubmit.co/mukherjee.sucheta90@email.com"
                 // method="POST"
+                onSubmit={handleSubmit}
             >
                 <p>
-                <span><input type="text" className="form-control" name="firstName" placeholder="First Name"/></span>
-                <span><input type="text" className="form-control" name="lastName" placeholder="Last Name"/></span>
+                <span><input type="text" onChange={handleChange} className="form-control" name="firstName" placeholder="First Name" required value={firstName}/></span>
+                <span><input type="text" onChange={handleChange} className="form-control" name="lastName" placeholder="Last Name" required value={lastName}/></span>
                 </p>
                 <p>
-                    <input type="email"placeholder="Email" className="form-control" name="email"/>
+                    <input type="email"onChange={handleChange} placeholder="Email" className="form-control" name="email" required value={email} />
                 </p>
                 <p>
-                    <textarea type="text" name="message" className="form-control" rows="3" placeholder="Type your message here"/>
+                    <textarea type="text" onChange={handleChange} name="message" value={message} className="form-control" rows="3" placeholder="Type your message here" required/>
                 </p>
-                <button type="button" className="btn" id="form-btn">Send</button>
+                <button className="btn" onChange={handleChange} id="form-btn">Send</button>
                 
             </form>
         </div>
