970987431af987749df92417a9b01f93d5b2a375
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-08-24 21:18:34 -0400

    imported google font for all h1

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,8 +9,8 @@ export default function AboutPage(){
                 <img src={ProfilePicture} className="img-fluid"/>
             </div>
             <div className="col-sm-12 col-md-6 d-flex flex-column align-items-center justify-content-center">
-            <h1>About Me </h1>
-                <p className="col"> I am a Frontend web developer based in Glen Allen, Virginia. My expertise lies in creating user-friendly web applications and websites, leveraging a skill set that encompasses HTML, CSS, JavaScript, and React. With proficiency in these languages, I strive to build exceptional online experiences.</p><p></p>
+                <h1>About Me </h1>
+                <p className="col"> I am a Frontend web developer based in Glen Allen, Virginia. My expertise lies in creating user-friendly web applications and websites, leveraging a skill set that encompasses HTML, CSS, JavaScript, and React. With proficiency in these languages, I strive to build exceptional online experiences.</p>
 
                 <p className="col" >During my prior employment in the Customer Service industry, I gained valuable experience in problem-solving. Upon relocating to the USA, I transitioned into the role of an office administrative assistant. This position provided me with a comprehensive understanding of crucial aspects such as organizational skills, time management, effective communication, rapid learning, critical thinking, and meticulous attention to detail. These experiences have shaped me into a well-rounded professional with a strong foundation in various essential skills.</p>
 
