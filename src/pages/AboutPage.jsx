e86eeb37138a348cb3fc9306c04083035a4db57c
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-09-12 12:54:01 -0400

    changed the about me section to make it more personal and changed style on the contact page to match the theme

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,16 +1,32 @@
-import ProfilePicture from "../images/profile.png"
-import './AboutPage.css';
+import ProfilePicture from "../images/profile.png";
+import "./AboutPage.css";
 
-export default function AboutPage(){
-    return (
-        <div className="p-5" id="about-page">
-        <div className="row d-flex justify-content-evenly align-items-center">
-            <div className="col-6 d-flex align-items-center" id="profile-img">
-                <img src={ProfilePicture} className="img-fluid"/>
-            </div>
-            <div className="col-sm-12 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                <h1>About Me </h1>
-                <p className="col"> 
+export default function AboutPage() {
+  return (
+    <div className="p-5" id="about-page">
+      <div className="row d-flex justify-content-evenly align-items-center">
+        <div className="col-6 d-flex align-items-center" id="profile-img">
+          <img src={ProfilePicture} className="img-fluid" />
+        </div>
+        <div className="col-sm-12 col-md-6 d-flex flex-column align-items-center justify-content-center">
+          <h1>About Me </h1>
+          <p>
+            Hello! I&#39;m Sucheta Mukherjee, a passionate web developer with a
+            diverse background. My journey began as a self-taught front-end
+            developer, and I&#39;ve since graduated from the Full Stack Bootcamp
+            at Southern Methodist University. I thrive on working with a wide
+            range of technologies, from HTML, CSS, JavaScript, and React on the
+            front end, to Node, Express, MySQL, MongoDB, GraphQL, Apollo Server,
+            and Apollo Client on the back end. My goal is to continue learning
+            and evolving as a developer, taking on exciting projects and
+            contributing to innovative solutions. I believe that the key to
+            success in web development is a combination of technical expertise,
+            curiosity, and adaptability. I&#39;m always up for a challenge and
+            ready to explore new horizons. Feel free to reach out if you&#39;d
+            like to connect or collaborate. Let&#39;s create something amazing
+            together!
+          </p>
+          {/* <p className="col"> 
                     As an aspiring web developer, I have acquired a strong foundation in frontend technologies, including HTML, CSS, JavaScript, and jQuery. I am skilled in creating responsive and visually appealing web interfaces. Moreover, I am proficient in working with popular CSS frameworks such as Bulma and Bootstrap, which enable me to build sleek and intuitive user interfaces efficiently.
                 </p>
 
@@ -23,10 +39,9 @@ export default function AboutPage(){
                 </p> 
                 <p className="col">
                 As I continue my journey as a web developer, I am excited to take on new challenges, learn from experienced professionals, and leverage my knowledge to build cutting-edge web applications that make a positive impact in the digital world.
-                </p>
-            </div>
-            
+                </p> */}
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,91 +1,121 @@
-import './ContactPage.css';
-import { useState } from 'react';
-import { validateEmail } from '../../utils/validation';
-import emailjs from '@emailjs/browser';
-import keys from '../../utils/keys';
+import "./ContactPage.css";
+import { useState } from "react";
+import { validateEmail } from "../../utils/validation";
+import emailjs from "@emailjs/browser";
+import keys from "../../utils/keys";
 
+export default function ContactPage() {
+  const contactForm = document.getElementById("contact-form");
+  // const contactForm = useRef();
+  const [showMessage, setShowMessage] = useState("");
+  const [userName, setUserName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-export default function ContactPage(){
-    const contactForm = document.getElementById('contact-form')
-    // const contactForm = useRef();
-    const [showMessage, setShowMessage] = useState("")
-    const [userName, setUserName] = useState("");
-    // const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-   
-       
-    const handleChange = (e)=>{
-       const {target} = e;
-       const inputType = target.name;
-       const inputValue = target.value;
-       if(inputType === "user_name"){
-        setUserName(inputValue);
-       }
-       
-       if(inputType === "user_email"){
-         setEmail(inputValue);
-       }
-       if(inputType === "message"){
-         setMessage(inputValue);
-       }
+  const handleChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+    if (inputType === "user_name") {
+      setUserName(inputValue);
     }
-    function handleSubmit(e){
 
-        e.preventDefault();
-        console.log('clicked')
-        const validEmail = validateEmail(email);
-        if(!validEmail){
-            setShowMessage( <div className="alert alert-danger my-2" role="alert">Please provide a valid email.</div>)
-        }else if(validEmail && userName && message){
-
-            emailjs.sendForm(keys.service, keys.template , contactForm, keys.some_key)
-           .then((result) => {
-          console.log(result.text);
-          setShowMessage(<div className="alert alert-success  my-2" role="alert">Thank You!!! You message has been sent</div>)
+    if (inputType === "user_email") {
+      setEmail(inputValue);
+    }
+    if (inputType === "message") {
+      setMessage(inputValue);
+    }
+  };
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("clicked");
+    const validEmail = validateEmail(email);
+    if (!validEmail) {
+      setShowMessage(
+        <div className="alert alert-danger my-2" role="alert">
+          Please provide a valid email.
+        </div>
+      );
+    } else if (validEmail && userName && message) {
+      emailjs
+        .sendForm(keys.service, keys.template, contactForm, keys.some_key)
+        .then(
+          (result) => {
+            console.log(result.text);
+            setShowMessage(
+              <div className="alert alert-success  my-2" role="alert">
+                Thank You!!! You message has been sent
+              </div>
+            );
             setUserName("");
             setEmail("");
             setMessage("");
-
-            }, (error) => {
-          console.log(error.text);
-            });
-            
-        }
-    
-       
-        
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
     }
-    return (
-        <div className="mx-auto py-5  d-flex flex-column justify-content-center align-items-center h-100 form-container" id="contact-page" >
-   
-          
-        <div className="card m-5 col-lg-5" >
-            <h1>Get In Touch</h1>
-            <form
-                className='my-5'
-                id = 'contact-form'
-                onSubmit={handleSubmit}
-            >
-                {/* <p>
+  }
+  return (
+    <div
+      className="mx-auto py-5  d-flex flex-column justify-content-center align-items-center h-100 form-container"
+      id="contact-page"
+    >
+      <div className="card m-5 col-lg-5">
+        <h1>Get In Touch</h1>
+        <form className="my-5" id="contact-form" onSubmit={handleSubmit}>
+          {/* <p>
                 <span><input type="text" onChange={handleChange} className="form-control" name="firstName" placeholder="First Name" required value={firstName}/></span>
                 <span><input type="text" onChange={handleChange} className="form-control" name="lastName" placeholder="Last Name" required value={lastName}/></span>
                 </p> */}
-                <p>
-                <input type="text" onChange={handleChange} className="form-control" name="user_name" value={userName} placeholder='Name'/>
-                </p>
-                <p>
-                    <input type="email"onChange={handleChange} placeholder="Email" className="form-control" name="user_email" required value={email} />
-                </p>
-                <p>
-                    <textarea type="text" onChange={handleChange} name="message" value={message} className="form-control" rows="3" placeholder="Type your message here" required/>
-                </p>
-                
-                <button className="btn rounded-pill px-5"  style={{backgroundColor: "#f76f8eff", color: "white", fontWeight:"bold"}}>Send</button>
-                
-            </form>
-        </div>
-        {showMessage}
-        </div>
-    )
+          <p>
+            <input
+              type="text"
+              onChange={handleChange}
+              className="form-control"
+              name="user_name"
+              value={userName}
+              placeholder="Name"
+            />
+          </p>
+          <p>
+            <input
+              type="email"
+              onChange={handleChange}
+              placeholder="Email"
+              className="form-control"
+              name="user_email"
+              required
+              value={email}
+            />
+          </p>
+          <p>
+            <textarea
+              type="text"
+              onChange={handleChange}
+              name="message"
+              value={message}
+              className="form-control"
+              rows="3"
+              placeholder="Type your message here"
+              required
+            />
+          </p>
+
+          <button
+            className="btn rounded-pill px-5 my-2"
+            style={{
+              backgroundColor: "#f76f8eff",
+            }}
+          >
+            <h3 className="text-light my-1">Send</h3>
+          </button>
+        </form>
+      </div>
+      {showMessage}
+    </div>
+  );
 }
