da5b0a1e51c5b43c9b674fe434ad7a9d7ab5a599
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-09-13 15:05:31 -0400

    updated resume" "

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,16 @@
 // import React from "react";
 // import Hero from "../../src//images/protfolio_background.avif"
 import "./HomePage.css";
-export default function HomePage(){
-    return (
-        <div id='hero-container'>
-            <div id="hero">
-              
-            </div>
-              
-          <h1 id="welcome">Hi, my name is Sucheta Mukherjee
-             
-             <p>I am a Full Stack Web Developer</p>
-             <p>Welcome to my page!!</p>
-           
-           </h1>
-        </div>
+export default function HomePage() {
+  return (
+    <div id="hero-container">
+      <div id="hero"></div>
 
-      )
+      <h1 id="welcome">
+        Hi, my name is Sucheta Mukherjee
+        <h2>I am a Full Stack Web Developer</h2>
+        <h2>Welcome to my page!!</h2>
+      </h1>
+    </div>
+  );
 }
-
