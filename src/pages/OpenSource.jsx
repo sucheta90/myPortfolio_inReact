58dcb7555b5ddf9be211aa632793f872f6322d27
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2024-01-15 11:06:01 -0500

    added open source contributions to the newly created page

--- src/pages/OpenSource.jsx
+++ src/pages/OpenSource.jsx
@@ -1,7 +1,50 @@
+import "./OpenSource.css";
 export default function OpenSourceContribution() {
   return (
-    <div>
-      <h2>Comming soon ... </h2>
+    <div style={{ backgroundColor: "#ffead0ff" }} className="h-100">
+      <h1>Open Source Contributions</h1>
+      <section style={{ height: "100vh" }}>
+        <ul className="mb-0 p-3">
+          <li className="contribution-list">
+            Replace the blue square self-scheduler for anyone with 5 or more
+            blue squares (anyone other than Owner/Admin){" "}
+            <button>
+              <a
+                href="https://github.com/OneCommunityGlobal/HighestGoodNetworkApp/pull/1715"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Open
+              </a>
+            </button>
+          </li>
+          <li className="contribution-list">
+            Fix Task Name entry-box formatting{" "}
+            <button>
+              <a
+                href="https://github.com/OneCommunityGlobal/HighestGoodNetworkApp/pull/1724"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Open
+              </a>
+            </button>
+          </li>
+          <li className="contribution-list">
+            Update Projects Page sorting (by Project Name) and Filters Project
+            List by Category Name and Active/ Inactive{" "}
+            <button>
+              <a
+                href="https://github.com/OneCommunityGlobal/HighestGoodNetworkApp/pull/1812"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Open
+              </a>
+            </button>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
